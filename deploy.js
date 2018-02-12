ab95814c2488fd32c9598641f89b87eebb60912c
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-02-11 19:52:49 -0800

    change number to string for handling large numbers with precision

--- deploy.js
+++ deploy.js
@@ -14,7 +14,7 @@ const deploy = async () => {
 
   const contractInstance = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ from: accounts[0], gas: 1000000 })
+    .send({ from: accounts[0], gas: '1000000' })
 
   console.log(contractInstance.options.address)
   // 0x60A2C6679fCb788DbD74EA8B8EC79A67c6311b3d
